5a829279a9020ef2ddfb0676c7087cd2ccaf1da0
Gachuri <rushionchegge@gmail.com>
2024-09-25 09:27:19 +0300

    Implemented Dog and Person classes with methods for barking and talking

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def bark(self):
+        """Prints 'Woof!'"""
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        """Prints 'The dog is sitting.'"""
+        print("The dog is sitting.")
